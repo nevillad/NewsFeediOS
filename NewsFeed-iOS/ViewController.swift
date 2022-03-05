6c2dbf11b5427af874cfea48e0a7fa8553206be6
Nevilkumar Lad <Nevilkumar.Lad@freecharge.com>
2022-03-05 17:11:18 +0530

    Initial Commit

---
+++ NewsFeed-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsFeed-iOS
+//
+//  Created by Nevilkumar Lad on 05/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
